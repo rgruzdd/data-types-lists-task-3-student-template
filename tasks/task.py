e0354ebe08689da4fb76ee17776acf0990ec4f94
Gruzin <gruzd.roman@inbox.ru>
2023-03-13 17:23:22 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -3,4 +3,15 @@ from typing import List
 
 def foo(nums: List[int]) -> List[int]:
     # TODO: Add your code here
-    return []
+    new_list = []
+    multiplication = 1
+    for i in nums:
+        multiplication *= i
+    for i in nums:
+        new_list.append(int(multiplication/i))
+
+
+
+    return new_list
+
+print(foo([1,2,3,4,5,6]))
